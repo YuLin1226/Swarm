a19ac33142913321df4ea69e53cc9ec85e659b6c
YuLin Chen <r08522622@ntu.edu.tw>
2020-07-02 17:27:28 +0800

    Adding Lidar

--- build/car_control/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yulin/swarm_ws/src/car_control
-# Build directory: /home/yulin/swarm_ws/build/car_control
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/yulin/swarm_ws/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/yulin/swarm_ws/devel;/home/yulin/precision_control/devel;/home/yulin/Delta/devel;/home/yulin/car_ws/devel;/home/yulin/box_ws/devel;/home/yulin/test_ws/devel;/opt/ros/kinetic".split(';') if "/home/yulin/swarm_ws/devel;/home/yulin/precision_control/devel;/home/yulin/Delta/devel;/home/yulin/car_ws/devel;/home/yulin/box_ws/devel;/home/yulin/test_ws/devel;/opt/ros/kinetic" != "" else []
