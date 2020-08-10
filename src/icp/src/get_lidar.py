2cefdffb17f264f91af44e25737f7608e7584c48
YuLin Chen <r08522622@ntu.edu.tw>
2020-08-10 12:17:11 +0800

    get lidar point , ok

---
+++ src/icp/src/get_lidar.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+import rospy
+import math
+import geometry_msgs.msg
+from std_msgs.msg import Float64
+from sensor_msgs.msg import LaserScan
+import tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+class LiDAR_Association():
+    def __init__(self, topic_name='/solamr_1/scan_lidar'):
+        rospy.init_node('get_laser_scan_data', anonymous=True)
+
+
+
+        
+        self.fig = plt.figure()
+        # self.vel_pub = rospy.Publisher('/solamr_1/cmd_vel', geometry_msgs.msg.Twist, queue_size=10)
+        # self.cmd = geometry_msgs.msg.Twist()
+        rospy.Subscriber(topic_name, LaserScan, self.get_lidar)
+        
+        
+
+    def get_lidar(self, msg):
+        point = []
+        for ind, dist in enumerate(msg.ranges):
+            if dist >= msg.range_min and dist <= msg.range_max:
+                
+                x = dist * math.cos(ind * math.pi / 725)
+                y = dist * math.sin(ind * math.pi / 725)
+                point.append([
+                    x,
+                    y
+                ])
+        self.px = [i[0] for i in point]
+        self.py = [i[1] for i in point]
+
+
+
+if __name__ == "__main__":
+    try:
+        a = LiDAR_Association()
+        b = LiDAR_Association()
+        rospy.spin()
+    except KeyboardInterrupt:
+        pass
+
+    finally:
+        print(b.px)
+        plt.scatter(a.px, a.py, s=20, color="black")
+        plt.scatter(b.px, b.py, s=5, color="red")
+        plt.show()
+        pass
--- src/icp/src/test.py
+++ src/icp/src/test.py
@@ -1,19 +1,35 @@
 #!/usr/bin/env python
-# import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import numpy as np
 
 
 
-x = np.linspace(0.5 , 3.5 , 100)
-y = np.sin(x)
-y1 = np.random.randn(100)
-i = 0
+# x = np.linspace(0.5 , 3.5 , 100)
+# y = np.sin(x)
+# y1 = np.random.randn(100)
+# i = 0
 
-a = np.array([0,1,3,4])
-x = np.array([
-    [1,3,5,7,9,11,13,15,17,19],
-    [2,3,4,5,6, 7, 8, 9,10,11]
-])
+# a = np.array([0,1,3,4])
+# x = np.array([
+#     [1,3,5,7,9,11,13,15,17,19],
+#     [2,3,4,5,6, 7, 8, 9,10,11]
+# ])
 
 # for i, s in enumerate(x.T):
     # print(i,s)
+
+x = np.linspace(0, 10, 100)
+y = np.cos(x)
+
+fig = plt.figure()
+
+for p in range(50):
+    p=3
+    updated_x=x+p
+    updated_y=np.cos(x)
+    plt.plot(updated_x,updated_y)
+    plt.draw()  
+    x=updated_x
+    y=updated_y
+    plt.pause(0.0002)
+    fig.clear()
