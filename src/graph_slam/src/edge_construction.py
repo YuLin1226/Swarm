0ca769cced652a65e97e5a52a4374f2e7a262225
YuLin1226 <chenyl@solab.me.ntu.edu.tw>
2020-08-24 23:05:48 +0800

    test the file, now and never

--- src/graph_slam/src/edge_construction.py
+++ src/graph_slam/src/edge_construction.py
@@ -166,7 +166,278 @@ class ICP():
         
 
 
+# Feature Detection
+class Segment():
+    def __init__(self, x, y):
+        '''
+        Parameters:
+        d_thres : for segmentation of the consecutive data points.
+        d_break_thres : for segmentation of the breakpoints. 
+        
+        '''
+        self.d_thres = 5
+        self.d_break_thres = 1
+        self.shape_thres = 0.07
+        self.R_thres = 1.5
+        
+        self.circle_thres = (math.pi/180)*60
+        self.corner_thres = 0.7
 
+        self.seg = []
+        self.do_segment(x, y)
+        self.seg = self.classify_segment_type(self.seg)
+        self.seg = self.corner_merger_operation(self.seg)
+        self.landmark = self.do_landmark(self.seg)
+
+
+    def do_segment(self, x, y):
+        '''
+        Input:
+        x: 1xn, ndarray
+        y: 1xn, ndarray
+        '''
+
+        for i in range(len(x)):
+            
+            d_sum = 0
+            for L in range(len(x) - 1):
+                d       = cal_dist( x[0] , y[0] , x[L+1] , y[L+1] )
+                d_conti = cal_dist( x[L] , y[L] , x[L+1] , y[L+1] )
+                d_sum   = d_sum + d_conti
+                
+                if (d_sum - d) > self.d_thres or L == (len(x) - 1) or d_conti > self.d_break_thres:
+                    
+                    self.seg.append([
+                        x[0:L+1],
+                        y[0:L+1],
+                        []
+                    ])
+                    x = x[L+1 : len(x)]
+                    y = y[L+1 : len(y)]
+
+                    break
+
+            if len(x) <= 2:
+                self.seg[-1][0] = self.seg[-1][0] + x
+                self.seg[-1][1] = self.seg[-1][1] + y
+                break
+    
+    def classify_segment_type(self, seg):
+        
+        # for loop : "number of row" times
+        for i in range( len(seg) ):
+            x = seg[i][0]
+            y = seg[i][1]
+
+            x0 = x[0]
+            y0 = y[0]
+
+            xn = x[-1]
+            yn = y[-1]
+
+            xm = np.mean(x)
+            ym = np.mean(y)
+
+            _, _, r = cal_circle([x0, xm, xn], [y0, ym, yn])
+            # print("Segment Radius is %f"%r)
+            if r > self.R_thres:
+                seg[i][2] = "line"
+                
+            else:
+                seg[i][2] = "circle"
+        
+        return seg
+                
+    def corner_merger_operation(self, seg):
+        k = 0
+        i = 0
+        for i in range(len(seg) - 1):
+            
+            i = i + 1 + k
+            k = 0
+
+            if i+1 >= len(seg):
+                break
+
+            x1 = seg[i][0]
+            y1 = seg[i][1]
+            # x1m = np.mean(x1)
+            # y1m = np.mean(y1)
+            
+            x2 = seg[i+1][0]
+            y2 = seg[i+1][1]
+            # x2m = np.mean(x2)
+            # y2m = np.mean(y2)
+
+            val = cal_acos([ x1[-1] - x1[0], y1[-1] - y1[0] ], [x2[-1] - x2[0], y2[-1] - y2[0]])
+
+            if seg[i][2] == "circle":
+            
+                # circle , circle
+                if seg[i][2] == seg[i+1][2]:
+                    # 合併Operation
+                    seg[i][0] = np.hstack(( seg[i][0] , seg[i+1][0] ))
+                    seg[i][1] = np.hstack(( seg[i][1] , seg[i+1][1] ))
+                    # seg[i][0] = seg[i][0] + seg[i+1][0]
+                    # seg[i][1] = seg[i][1] + seg[i+1][1]
+                    seg.pop(i+1)
+                    k = -1
+
+                # circle , line   
+                else:                       
+                    # 判斷轉角
+                    if val > self.corner_thres:
+                        k = 0
+                        
+                    # 合併circle (後者理當是Circle，但是誤判成Line的情況)
+                    else:
+                        
+                        # 合併Operation
+                        seg[i][0] = np.hstack(( seg[i][0] , seg[i+1][0] ))
+                        seg[i][1] = np.hstack(( seg[i][1] , seg[i+1][1] ))
+                        # seg[i][0] = seg[i][0] + seg[i+1][0]
+                        # seg[i][1] = seg[i][1] + seg[i+1][1]
+                        seg.pop(i+1)
+                        k = -1
+            # seg[i][2] == "line"    
+            else:
+                
+                # line , line
+                if seg[i][2] == seg[i+1][2]:  
+                    
+                    
+                    # 判斷轉角
+                    if val > self.corner_thres:  
+                        pass
+                        
+                        
+                    # 合併line
+                    else:
+                        
+                        # 合併Operation
+                        seg[i][0] = np.hstack(( seg[i][0] , seg[i+1][0] ))
+                        seg[i][1] = np.hstack(( seg[i][1] , seg[i+1][1] ))
+                        # seg[i][0] = seg[i][0] + seg[i+1][0]
+                        # seg[i][1] = seg[i][1] + seg[i+1][1]
+                        seg.pop(i+1)
+                        k = -1
+                        
+                    
+                # line , circle
+                else:                      
+                    
+                    
+                    # 判斷轉角
+                    if val > self.corner_thres:
+                        pass
+                        
+                        
+                    # 合併line (後者理當是Line，但是誤判成Circle的情況)
+                    else:
+                        
+                        # 合併Operation
+                        seg[i][0] = np.hstack(( seg[i][0] , seg[i+1][0] ))
+                        seg[i][1] = np.hstack(( seg[i][1] , seg[i+1][1] ))
+                        # seg[i][0] = seg[i][0] + seg[i+1][0]
+                        # seg[i][1] = seg[i][1] + seg[i+1][1]
+                        seg.pop(i+1)
+                        k = -1
+        return seg
+
+    def do_landmark(self, seg):
+        circle_num = 0
+        for i in range(len(seg)):
+            if seg[i][2] == "circle":
+                circle_num = circle_num + 1
+            
+        corner_num = 0
+        for i in range( len(seg) - 1 ):
+            
+            x1 = seg[i][0][-1]
+            y1 = seg[i][1][-1]
+            x2 = seg[i+1][0][0]
+            y2 = seg[i+1][1][0]
+
+            d = cal_dist(x1,y1,x2,y2)
+            if d < self.d_break_thres:
+                corner_num = corner_num + 1
+            
+
+
+        landmark =  [   ["corner",    corner_num],
+                        ["circle",  circle_num] ]
+        return landmark
+
+def cal_acos(v1, v2):
+    '''
+    Input:
+    v1 : Input vector.
+    v2 : Input vector.
+    
+    Output:
+    value : Angle between these two vectors. (unit: rad)
+    '''
+    d1 = (v1[0]**2 + v1[1]**2)**(0.5)
+    d2 = (v2[0]**2 + v2[1]**2)**(0.5)
+    
+    value = math.acos( (v1[0]*v2[0]+v1[1]*v2[1])/(d1*d2) )
+    return value
+
+def cal_circle(x, y):
+    '''
+    Input:
+    x : List or array which stores 3 data respectively.
+    y : List or array which stores 3 data respectively.
+    
+    Output:
+    xc : X coordinate of the center.
+    yc : X coordinate of the center.
+    R  : Radius of the center.
+    '''
+
+    A = np.array([
+        [ 2*x[0], 2*y[0], 1 ],
+        [ 2*x[1], 2*y[1], 1 ],
+        [ 2*x[2], 2*y[2], 1 ]
+    ])
+    B = np.array([
+        [ x[0]**2 + y[0]**2 ],
+        [ x[1]**2 + y[1]**2 ],
+        [ x[2]**2 + y[2]**2 ]
+    ])
+
+    X = np.linalg.solve(A, B)
+    xc = X[0]
+    yc = X[1]
+    R  = ( X[2] + xc**2 + yc**2 )**(0.5)
+    return xc, yc, R
+
+
+def cal_dist(xi, yi, xf, yf):
+    
+    dx = xi - xf
+    dy = yi - yf
+    
+    return (dx**2 + dy**2)**(0.5)
+
+def read_csv(csv_file_scan='case1.csv'):
+    
+    '''
+    Read the scan data set from the csv file.
+    '''
+    with open(csv_file_scan) as scan:
+        scan_csv = csv.reader(scan)
+        scan_list = []
+        # Data from csv file are "string", 
+        # so they need to be change to float type by "list(map(float, row))" operation.
+        for row in scan_csv:
+            x = list(map(float, row))
+            scan_list.append(x)
+        scan_set = np.array(scan_list)
+    
+    return scan_set
+
+# 
 
 
 class LiDAR_Association():
@@ -176,10 +447,11 @@ class LiDAR_Association():
         # self.vel_pub = rospy.Publisher('/solamr_1/cmd_vel', geometry_msgs.msg.Twist, queue_size=10)
         # self.px = []
         # self.py = []
-        self.scan = np.zeros([])
+        # self.scan = np.zeros([])
         rospy.Subscriber(topic_name, LaserScan, self.get_lidar)
         
     def get_lidar(self, msg):
+        
         point = []
         for ind, dist in enumerate(msg.ranges):
             if dist >= msg.range_min and dist <= msg.range_max:
@@ -192,7 +464,15 @@ class LiDAR_Association():
                 ])
         # self.px = [i[0] for i in point]
         # self.py = [i[1] for i in point]
-        self.scan = np.array(point)
+        scan = np.array(point)
+        
+        px = scan[:,0]
+        py = scan[:,1]
+        Seg = Segment(px, py)
+        self.landmark = Seg.landmark
+        print(self.landmark)
+        print("=============")
+        print("")
         # print(np.array(point).shape)
         
 
@@ -211,14 +491,14 @@ if __name__ == "__main__":
 
     try:
         car1 = LiDAR_Association('/solamr_1/scan_lidar')
-        car2 = LiDAR_Association('/solamr_2/scan_lidar')
+        # car2 = LiDAR_Association('/solamr_2/scan_lidar')
         
         scan_list = []
         Edge = []
         while not rospy.is_shutdown():
             
-            scan_list.append(car1.scan)
-            scan_list.append(car2.scan)
+            # scan_list.append(car1.scan)
+            # scan_list.append(car2.scan)
             
             # if len(scan_list) >= 3:
                     
@@ -229,16 +509,18 @@ if __name__ == "__main__":
             #     pose = t2v(T)
             #     print(pose)
 
-            if len(scan_list) >= 10:
+            # if len(scan_list) >= 10:
                     
-                A = scan_list[-1]
-                B = scan_list[-2]
+            #     A = scan_list[-1]
+            #     B = scan_list[-2]
                 
-                T,_ = ICP().icp(A,B)
-                pose = t2v(T)
-                print("x: %f"%pose[0])
-                print("y: %f"%pose[1])
-                print("yaw: %f"%pose[2])
+            #     T,_ = ICP().icp(A,B)
+            #     pose = t2v(T)
+            #     print("x: %f"%pose[0])
+            #     print("y: %f"%pose[1])
+            #     print("yaw: %f"%pose[2])
+
+            
             rate.sleep()
 
         rospy.spin()
---
+++ src/graph_slam/src/test.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+import rospy
+import numpy as np
+import math
+import csv
+import matplotlib.pyplot as plt
+from sensor_msgs.msg import LaserScan
+
+
+
+
+class LiDAR_Association():
+
+    def __init__(self, topic_name='/solamr_1/scan_lidar'):
+        
+        # self.vel_pub = rospy.Publisher('/solamr_1/cmd_vel', geometry_msgs.msg.Twist, queue_size=10)
+        # self.px = []
+        # self.py = []
+        self.scan = np.zeros([])
+        rospy.Subscriber(topic_name, LaserScan, self.get_lidar)
+        self.po = []
+
+
+    def get_lidar(self, msg):
+        point = []
+        self.po.append(msg.ranges)
+        for ind, dist in enumerate(msg.ranges):
+            if dist >= msg.range_min and dist <= msg.range_max:
+                
+                x = dist * math.cos(ind * math.pi / 725)
+                y = dist * math.sin(ind * math.pi / 725)
+                point.append([
+                    x,
+                    y
+                ])
+        # self.px = [i[0] for i in point]
+        # self.py = [i[1] for i in point]
+        self.scan = np.array(point)
+        # print(np.array(point).shape)
+
+
+if __name__ == "__main__":
+
+    rospy.init_node('node_edge_construction', anonymous=True)
+
+    try:
+        car1 = LiDAR_Association('/solamr_1/scan_lidar')
+        rospy.spin()
+
+    except KeyboardInterrupt:
+        pass
+
+    finally:
+        data = car1.po[-1]
+        x = []
+        y = []
+        for ind, dist in enumerate(data):
+            x.append(ind)
+            y.append(dist)
+        # x = np.array(x)
+        # y
+        plt.scatter(x,y, s=5, color="red")
+        plt.show()
+        pass
